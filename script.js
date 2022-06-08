383f82c41017f9c93eca9f4d4d94855838cfa038
onarman <onarmannn@gmail.com>
2022-06-08 17:17:02 +0300

    next step

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const input = document.querySelector("#input");
 const adder = document.querySelector(".add-task");
 const task = document.querySelector(".task");
 const ul = document.querySelector("#ul");
+const p = document.querySelector("p");
 
-adder.addEventListener("click",addTask)
+
+adder.addEventListener("click",addTask);
+input.addEventListener("keydown",(e)=>{
+    if (e.key == "Enter"){
+        adder.click()
+    }
+})
+
+
 function addTask(){
     if(input.value == ""){
         alert("Enter a task please!!!")
@@ -11,8 +20,19 @@ function addTask(){
         ul.innerHTML += `<li class="li-task"><i class="fa-solid fa-check fa-2x"></i>
         <p>${input.value}</p>
         <i class="fa-solid fa-trash-can fa-lg"></i></li>`
+        input.value="";
     }
+
 }
+
+ul.addEventListener("click",(event)=>{
+    // console.log(event.target);
+    if(event.target.classList.contains("fa-check")){
+        event.target.nextElementSibling.classList.toggle("p-task");
+    }else if(event.target.classList.contains("fa-trash-can")){
+        event.target.closest(".li-task").remove()
+    }else if(event.target.classList.contains("ul"))
+})
 
 
 
